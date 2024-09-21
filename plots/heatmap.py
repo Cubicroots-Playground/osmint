5ffb5fd748c86898f7a246a2e5ed6a2866d0711e
cubicroot <github@cubicroot.xyz>
2024-09-21 13:04:14 +0200

    auto zoom heatmap

--- plots/heatmap.py
+++ plots/heatmap.py
@@ -1,6 +1,8 @@
 import plotly.express as px
 import pandas
 
+from plots import autozoom
+
 # heatmap plots a heatmap based on the given data frame.
 #
 # The dataframe needs columns 'lat', 'lon' and 'cnt'.
@@ -8,16 +10,35 @@ def plot(
         df: pandas.DataFrame,
         img_out: str = None,
         ):
+    max_lon = -200
+    min_lon = 200
+    max_lat = -200
+    min_lat = 200
+    for _, row in df.iterrows():
+        if row.lat > max_lat:
+            max_lat = row.lat
+        if row.lat < min_lat:
+            min_lat = row.lat
+        if row.lon > max_lon:
+            max_lon = row.lon
+        if row.lon < min_lon:
+            min_lon = row.lon
+
+    center = autozoom.center_from_coordinates(max_lon=max_lon, min_lon=min_lon, max_lat=max_lat, min_lat=min_lat)
     fig = px.density_mapbox(df, 
                         lat="lat", 
                         lon="lon", 
                         z="cnt",
                         radius=10,
-                        zoom=2,
+                        zoom=autozoom.from_coordinates(max_lon=max_lon, min_lon=min_lon, max_lat=max_lat, min_lat=min_lat),
+                        center=dict(lat=center[1], lon=center[0])
                         )
 
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_layout(geo = dict(
+            projection_scale=10
+        ))
 
     if img_out:
         fig.write_image(img_out, width=2250, height=1500)
